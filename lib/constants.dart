24400d3ce0359f9903f112a7288754c9ebab4277
niverovskiinn <heket844@gmail.com>
2020-06-25 13:16:51 +0300

    Init commit

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+final String title = "Color App";
+
+final MaterialColor primaryColor = Colors.blue;
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import 'package:test_app_flutter/constants.dart';
+import 'package:test_app_flutter/homepage.dart';
+
+/*
+
+TODO
+  1. Install Flutter
+  2. Create a test application
+  3. The application should: display the text "Hey there" in the middle of the
+    screen and after tapping anywhere on the screen a background color should be
+    changed to a random color. You can also add any other feature to the app -
+    that adds bonus points
+  4. Please do not use any external libraries for color generation
+  5. Push code to a GitHub
+  6. Send the link to a GitHub repository back
+
+ */
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: title,
+      theme: ThemeData(
+        primarySwatch: primaryColor,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
